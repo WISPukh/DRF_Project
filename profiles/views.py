d657f7b5befbbf8ea32c03a78706af9204944bc3
FeatherGF <o.pukhov@wis.software>
2022-09-21 16:27:54 +0300

    [bug_fix] Fix profile updating issue

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,3 @@
-from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
@@ -14,7 +13,5 @@ class ProfileDetailViewSet(ModelViewSet):
     def get_queryset(self):
         return self.model.objects.filter(user_id=self.kwargs.get('pk'))
 
-    @action(detail=True, methods=['put'])
     def put(self, request, *args, **kwargs):
-        profile = self.model.objects.get(user_id=self.kwargs.get('pk'))
         return self.update(request, *args, **kwargs)
