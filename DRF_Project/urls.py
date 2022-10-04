53b04be86d2c048a6da2d5575a7582dba069960b
FeatherGF <o.pukhov@wis.software>
2022-10-04 12:03:30 +0300

    [fix] delete media

--- DRF_Project/urls.py
+++ DRF_Project/urls.py
@@ -42,6 +42,3 @@ urlpatterns = [
     path('api/orders/history/', include(orders.urls)),
     path('api/cart/', include(cart.urls)),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL)
