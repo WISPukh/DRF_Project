cdbba37872f2a375359408dcf9fe50be355228d5
FeatherGF <o.pukhov@wis.software>
2022-10-04 12:07:02 +0300

    [fix] add media

--- DRF_Project/urls.py
+++ DRF_Project/urls.py
@@ -42,3 +42,6 @@ urlpatterns = [
     path('api/orders/history/', include(orders.urls)),
     path('api/cart/', include(cart.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL)
